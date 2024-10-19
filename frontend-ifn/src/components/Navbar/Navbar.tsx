4c15ccdc73c7d5baaf71296307a56198058c36ec
Stefan Nicoara <nicoarastefan78@gmail.com>
2024-10-19 17:11:17 +0300

    actualizare favicon

--- frontend-ifn/src/components/Navbar/Navbar.tsx
+++ frontend-ifn/src/components/Navbar/Navbar.tsx
@@ -3,12 +3,18 @@ import SearchBar from "./SearchBar";
 import NotificationsButton from "./NotificationsButton";
 import UserMenuButton from "./UserMenuButton";
 import "./Navbar.scss";
+import logo from "../../assets/AccessCapitalLogo.png"; // Asigurați-vă că aveți un fișier logo.png în folderul assets
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
       <div className="navbar__container">
         <div className="navbar__logo">
+          <img
+            src={logo}
+            alt="Access Capital Logo"
+            className="navbar__logo-image"
+          />
           <span className="navbar__logo-text">Access Capital</span>
         </div>
         <div className="navbar__search">
