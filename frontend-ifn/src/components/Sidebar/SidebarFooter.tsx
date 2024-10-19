dfb31cef46d634fd5bfd1704e6c0939a886e1c19
Stefan Nicoara <nicoarastefan78@gmail.com>
2024-10-19 17:58:49 +0300

    update sidebarfooter

--- frontend-ifn/src/components/Sidebar/SidebarFooter.tsx
+++ frontend-ifn/src/components/Sidebar/SidebarFooter.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SidebarFooter: React.FC = () => {
-  return <div className="sidebar__footer"></div>;
+  return (
+    <div className="sidebar__footer">
+      {/* Conținutul footer-ului a fost eliminat, dar componenta rămâne activă */}
+    </div>
+  );
 };
 
 export default SidebarFooter;
